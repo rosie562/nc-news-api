fc6f27e32d0493ecbd31c0985fc09d89b0f3b759
rosie562 <rosieclairegarrett@gmail.com>
2023-11-20 16:59:21 +0000

    adds controller and model functions for get /api endpoint to read data from endpoint.json file

--- controllers/news.controllers.js
+++ controllers/news.controllers.js
@@ -1,9 +1,17 @@
-const { selectTopics } = require("../models/news.models");
+const { selectTopics, readEndpoint } = require("../models/news.models");
 
 exports.getTopics = (req, res, next) => {
   selectTopics()
     .then((topics) => {
       res.status(200).send({ topics });
+    })
+    .catch(next);
+};
+
+exports.getEndpoints = (req, res, next) => {
+  readEndpoint()
+    .then((endpoint) => {
+      res.status(200).send({ endpoint });
     })
     .catch(next);
 };
--- models/news.models.js
+++ models/news.models.js
@@ -1,7 +1,16 @@
 const db = require("../db/connection");
+const endpoints = require("../endpoints.json");
+const fs = require("fs/promises");
 
 exports.selectTopics = () => {
   return db.query(`SELECT * FROM topics;`).then(({ rows }) => {
     return rows;
   });
 };
+
+
+exports.readEndpoint = () => {
+     return fs.readFile("endpoints.json").then((endpoints) => {
+       return JSON.parse(endpoints);
+     });
+}
