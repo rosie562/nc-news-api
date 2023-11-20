eeb0a7a4600cdc16dce617a0b8dc11c9bfe7114f
rosie562 <rosieclairegarrett@gmail.com>
2023-11-20 15:06:49 +0000

    remove app.use(express.json()) until it is needed for post and patch reqs

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const { getTopics } = require("./controllers/news.controllers");
 const { handleServerErrors } = require("./errors");
 
-app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
