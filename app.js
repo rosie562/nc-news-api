eaf41ab6227cbdb1bff3869901bb25fb49d35d48
rosie562 <rosieclairegarrett@gmail.com>
2023-11-20 13:32:35 +0000

    tests happy path for GET /api/topics and tests that a 404 is returned if path is not found

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
+const { getTopics } = require("./controllers/news.controllers");
+const {
+  handlePSQErrors,
+  handleCustomErrors,
+  handleServerErrors,
+} = require("./errors");
 
 app.use(express.json());
+
+app.get("/api/topics", getTopics);
+
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "path not found" });
+});
+
+app.use(handlePSQErrors);
+app.use(handleCustomErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
