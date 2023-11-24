33b03de233a62e283a42f828ede4a1026ab583d6
rosie562 <rosieclairegarrett@gmail.com>
2023-11-24 14:25:46 +0000

    adds tests for sort_by and order query

--- controllers/news.controllers.js
+++ controllers/news.controllers.js
@@ -49,8 +49,8 @@ exports.getCommentByArticleId = (req, res, next) => {
 };
 
 exports.getAllArticles = (req, res, next) => {
-  const { topic } = req.query;
-  selectArticles(topic)
+  const { topic, sort_by, order } = req.query;
+  selectArticles(topic, sort_by, order)
     .then((articles) => {
       res.status(200).send({ articles });
     })
